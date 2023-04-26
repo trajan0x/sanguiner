f3fd414938e7ce82cadeb92a4eabcbc268a031d8
defi-moses <jakedinero@protonmail.com>
2023-04-26 19:13:38 -0400

    clean up and revert

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import { JsonRpcProvider } from '@ethersproject/providers';
-import { Provider } from '@ethersproject/abstract-provider'
 import { SynapseSDK } from '@synapsecns/sdk-router';
 import { BigNumber } from '@ethersproject/bignumber';
 import express from 'express';
@@ -14,7 +13,7 @@ const port = process.env.PORT || 3000;
 
 //Basic hello world
 app.get('/', (req, res) => {
-  res.send(allChains,rpcProviders)
+  res.send('Welcome to the Synapse Rest API for swap and bridge quotes')
 });
 
 //Setting up arguments
@@ -66,44 +65,44 @@ app.listen(port, () => {
 });
 
 
-export const allChains = [
-  1,
-  10,
-  56,
-  250,
-  288,
-  1284,
-  1285,
-  137,
-  43144,
-  53935,
-  42161,
-  1313161554,
-  1666600000,
-  25,
-  1088,
-  8217,
-  2000,
-  7700
-]
+// export const allChains = [
+//   1,
+//   10,
+//   56,
+//   250,
+//   288,
+//   1284,
+//   1285,
+//   137,
+//   43144,
+//   53935,
+//   42161,
+//   1313161554,
+//   1666600000,
+//   25,
+//   1088,
+//   8217,
+//   2000,
+//   7700
+// ]
 
-export const rpcProviders = [
-  new JsonRpcProvider('https://rpc.ankr.com/eth'),
-  new JsonRpcProvider('https://rpc.ankr.com/optimism'),
-  new JsonRpcProvider('https://bsc-dataseed1.ninicoin.io/'),
-  new JsonRpcProvider('https://rpc.ftm.tools'),
-  new JsonRpcProvider('https://replica-oolong.boba.network/'),
-  new JsonRpcProvider('https://rpc.api.moonbeam.network'),
-  new JsonRpcProvider('https://rpc.api.moonriver.moonbeam.network'),
-  new JsonRpcProvider('https://rpc-mainnet.matic.quiknode.pro'),
-  new JsonRpcProvider('https://api.avax.network/ext/bc/C/rpc'),
-  new JsonRpcProvider('https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc'),
-  new JsonRpcProvider('https://arb1.arbitrum.io/rpc'),
-  new JsonRpcProvider('https://mainnet.aurora.dev'),
-  new JsonRpcProvider('https://harmony-mainnet.chainstacklabs.com'),
-  new JsonRpcProvider('https://evm-cronos.crypto.org'),
-  new JsonRpcProvider('https://andromeda.metis.io/?owner=1088'),
-  new JsonRpcProvider('https://klaytn.blockpi.network/v1/rpc/public'),
-  new JsonRpcProvider('https://rpc.ankr.com/dogechain'),
-  new JsonRpcProvider('https://mainnode.plexnode.org:8545'),
-]
+// export const rpcProviders = [
+//   new JsonRpcProvider('https://rpc.ankr.com/eth'),
+//   new JsonRpcProvider('https://rpc.ankr.com/optimism'),
+//   new JsonRpcProvider('https://bsc-dataseed1.ninicoin.io/'),
+//   new JsonRpcProvider('https://rpc.ftm.tools'),
+//   new JsonRpcProvider('https://replica-oolong.boba.network/'),
+//   new JsonRpcProvider('https://rpc.api.moonbeam.network'),
+//   new JsonRpcProvider('https://rpc.api.moonriver.moonbeam.network'),
+//   new JsonRpcProvider('https://rpc-mainnet.matic.quiknode.pro'),
+//   new JsonRpcProvider('https://api.avax.network/ext/bc/C/rpc'),
+//   new JsonRpcProvider('https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc'),
+//   new JsonRpcProvider('https://arb1.arbitrum.io/rpc'),
+//   new JsonRpcProvider('https://mainnet.aurora.dev'),
+//   new JsonRpcProvider('https://harmony-mainnet.chainstacklabs.com'),
+//   new JsonRpcProvider('https://evm-cronos.crypto.org'),
+//   new JsonRpcProvider('https://andromeda.metis.io/?owner=1088'),
+//   new JsonRpcProvider('https://klaytn.blockpi.network/v1/rpc/public'),
+//   new JsonRpcProvider('https://rpc.ankr.com/dogechain'),
+//   new JsonRpcProvider('https://mainnode.plexnode.org:8545'),
+// ]
