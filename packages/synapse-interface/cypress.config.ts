e1dfe7623705893b21400c02d9c358e7e7a1feb6
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-05-23 22:10:38 -0700

    synpress config setup, debug metamask boot error

--- packages/synapse-interface/cypress.config.ts
+++ packages/synapse-interface/cypress.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'cypress'
+import synpressPlugins from '@synthetixio/synpress/plugins'
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
+    setupNodeEvents: (on, config) => {
+      synpressPlugins(on, config)
+    },
+    supportFile: 'cypress/support/e2e.ts',
   },
   video: false,
   trashAssetsBeforeRuns: true,
   screenshotOnRunFailure: true,
   screenshotsFolder: 'cypress/visual-states/current-screenshots',
   videosFolder: 'cypress/visual-states/current-videos',
-})
+});
